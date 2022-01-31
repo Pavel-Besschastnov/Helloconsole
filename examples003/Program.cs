ad132cdf4da2dd9b465e447f581a61191287caf7
Dogora <Ddogora@gmail.com>
2022-02-01 02:27:37 +0300

    добавил случайные числа

--- examples003/Program.cs
+++ examples003/Program.cs
@@ -1,8 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
-int numberA = 3;
-int numberB = 22;
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 
 Console.WriteLine ( result );
