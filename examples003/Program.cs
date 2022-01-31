9d705dc37d31aacb7d925d63ba0583c7798d16e8
Dogora <Ddogora@gmail.com>
2022-02-01 02:01:36 +0300

    сделал 3 задание

---
+++ examples003/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numberA = 3;
+int numberB = 22;
+int result = numberA + numberB;
+
+Console.WriteLine ( result );
