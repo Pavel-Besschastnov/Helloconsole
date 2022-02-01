310e2cdb78e95151db2e253c9362c60b88d6cb7d
Dogora <Ddogora@gmail.com>
2022-02-01 06:53:12 +0300

    сохранил задачки

---
+++ Tasks/Block-1/task0/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numb = Int32.Parse(Console.ReadLine());
+
+numb= numb * numb;
+
+Console.Write("Квадрат введенного числа равен:  ");
+Console.WriteLine(numb);
+
+
+
+
---
+++ Tasks/Block-1/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int numbA = 4;
+int numbB = 16;
+int x = numbB * numbB;
+
+if(x == numbA)
+{
+    Console.WriteLine("первое число является квадратом второго");
+}
+
+else 
+{
+    Console.WriteLine("первое число не является квадратом второго числа");
+}
