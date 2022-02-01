47a7746a69fe7813006412bb0c88488875f7e8e6
Dogora <Ddogora@gmail.com>
2022-02-01 03:19:39 +0300

    маленькая задача

---
+++ examples006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 4;
+int c = 12;
+int d = 5;
+int e = 1;
+
+int max = a;
+
+if ( a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("max = ");
+Console.WriteLine (max);
