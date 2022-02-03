0b4986a1f899c1c1606ab394174277e1faba669d
Dogora <Ddogora@gmail.com>
2022-02-03 21:00:45 +0300

    задача 7

---
+++ Tasks/Block-1/Task7/Program.cs
@@ -0,0 +1,8 @@
+﻿int numbA = Convert.ToInt32(Console.ReadLine()); // Отрицательное число
+int numbB = Convert.ToInt32(Console.ReadLine()); // Положительное число
+int start = numbA;
+while (start < numbB + 1)
+{
+    Console.Write( start);
+    start++;
+}
