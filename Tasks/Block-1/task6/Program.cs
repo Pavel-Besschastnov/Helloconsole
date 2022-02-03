25b9955b87433127c285db83ae0bb8db63e28da6
Dogora <Ddogora@gmail.com>
2022-02-03 20:49:40 +0300

    задача 6

---
+++ Tasks/Block-1/task6/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int x = number % 2;
+if ( x == 0)
+{
+ Console.WriteLine("Число является четным");
+}
+else 
+{
+    Console.WriteLine("Число не четное");
+}
