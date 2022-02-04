1389c0265f5b1fa9657825c6267fc4b5cd988c4a
Dogora <Ddogora@gmail.com>
2022-02-04 05:28:25 +0300

    задача 11

---
+++ Tasks/Block-1/task11/Program.cs
@@ -0,0 +1,16 @@
+﻿int numb = new Random().Next(10, 99);
+Console.WriteLine(numb);
+int numbA = numb / 10;
+int numbB = numb % 10;
+if (numbA > numbB)
+{
+    Console.WriteLine($"Наибольшая цифра числа :  {numbA}");
+}
+else if (numbB > numbA)
+{
+    Console.WriteLine($"Наибольшая цифра числа : {numbB}");
+}
+else
+{
+    Console.WriteLine("Числа равны.");
+}
