79616e1c6ce07578026ccd91efe6aad51ca7cc4d
Dogora <Ddogora@gmail.com>
2022-02-04 04:12:50 +0300

    создание метода

---
+++ metod/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+int[] array = new int[10];
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int posicion = 0;
+    while (posicion < count)
+    {
+        Console.WriteLine(col[posicion]);
+        posicion++;
+    }
+}
+
+int Indexoff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int posicion=0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            posicion = index;
+        }
+        index++;
+    }
+    return posicion;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = Indexoff(array,5);
+Console.WriteLine(pos);
