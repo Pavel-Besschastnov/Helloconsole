0c383fd0af1397bef99403bb8ef245731852cd76
Dogora <Ddogora@gmail.com>
2022-02-07 05:03:15 +0300

    задача 13

---
+++ Tasks/Block-1/Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+WriteLine("Введите числа джля сравнения:  ");
+int A = Convert.ToInt32(ReadLine());
+int B = Convert.ToInt32(ReadLine());
+if ((A % B) == 0 )
+{
+    WriteLine( "Число A Кратно B");
+}
+else
+{
+    WriteLine($" Остаток от деления : {A % B}");
+}
