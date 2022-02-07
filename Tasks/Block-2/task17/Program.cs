29d2f484ca3b489526cd7ac4037aec4f69b0263c
Dogora <Ddogora@gmail.com>
2022-02-07 21:10:41 +0300

    17 и 19 задачи

---
+++ Tasks/Block-2/task17/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 17 По двум заданным числам проверять является ли одно квадратом другого
+using static System.Console;
+
+int numberA = new Random().Next(2,5);
+int numberB = new Random().Next(2,3);
+
+WriteLine($"{numberA},{numberB}");
+
+if (numberA == numberB * numberB)
+{
+    WriteLine( " первое число является квадратом второго");
+}
+else if (numberB == numberA * numberA)
+{
+    WriteLine( "Второе число является квадратом первого числа");
+}
+else
+{
+    WriteLine(  " Ни одно число не является квадратом другого.");
+}
---
+++ Tasks/Block-2/task19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19
+// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+using static System.Console;
+
+int x = new Random().Next(-10, 10);
+int y = new Random().Next(-10, 10);
+WriteLine(x);
+WriteLine(y);
+if (x == 0 || y == 0)
+{
+    WriteLine(" Ошибка"); return;
+}
+if (x > 0 && y > 0)
+{
+    WriteLine("Первая четверть");
+}
+if (x < 0 && y > 0)
+{
+    WriteLine(" Вторая четверть");
+}
+if (x < 0 && y < 0)
+{
+    WriteLine(" Третья четверть");
+}
+else if(x > 0 && y < 0) 
+{
+    WriteLine(" Четвертая четверть");
+}
+
