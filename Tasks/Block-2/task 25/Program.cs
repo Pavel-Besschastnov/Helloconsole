c4524d6e1bb1927f0ef664013e007533d6e654b6
Dogora <Ddogora@gmail.com>
2022-02-07 21:49:02 +0300

    23 и 21 и 25 задачи

---
+++ Tasks/Block-2/task 25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25
+// Найти сумму чисел от 1 до A
+
+using static System.Console;
+
+int A= new Random().Next(1,20);
+WriteLine($"N = {A}");
+int result =0;
+for (int i = 0; i < A+1; i++)
+{
+    result=result + i;
+}
+WriteLine( $"Сумма чисел ={result}");
