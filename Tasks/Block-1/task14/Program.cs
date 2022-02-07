de7fc31a81e490908e023d85b6161c73ae3cf4b7
Dogora <Ddogora@gmail.com>
2022-02-07 05:24:07 +0300

    Задача 14

---
+++ Tasks/Block-1/task14/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+WriteLine("Введите число");
+String number = ReadLine();
+WriteLine();
+if (number.Length < 3)
+{
+    WriteLine(" 3 числа не существует");
+}
+else
+{
+    WriteLine("3 числом по порядку является: ");
+    WriteLine(number[2]);
+}
