a45cfa9ea57a92be9332028c0b8d62b9b0c04862
Dogora <Ddogora@gmail.com>
2022-02-10 06:50:57 +0300

    24 задача

---
+++ Tasks/Block-2/task 24/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 24
+//Найти кубы чисел от 1 до N
+
+using static System.Console;
+
+
+int N= new Random().Next(1,20);
+WriteLine($"N = {N}");
+
+for (int i = 0; i < N+1; i++)
+{
+    int temp = i * i;
+    WriteLine($"N ^ 2 = {temp}");
+
+}
+
