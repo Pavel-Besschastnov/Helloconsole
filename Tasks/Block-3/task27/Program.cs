2868235a5f70284dc361832df9de632bd977161a
Dogora <Ddogora@gmail.com>
2022-02-11 20:59:09 +0300

    27 задача

---
+++ Tasks/Block-3/task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27
+// Определить количество цифр в слове
+
+using static System.Console;
+WriteLine("введите слово с цифрами: ");
+string ss = ReadLine();
+int count = 0;
+int a;
+for (int i = 0; i < ss.Length; i++)
+{
+    bool result = int.TryParse((ss[i].ToString()),out a);
+    if (result == true)
+        count++;
+}
+
+WriteLine($"Количество цифр в слове : {count}");
