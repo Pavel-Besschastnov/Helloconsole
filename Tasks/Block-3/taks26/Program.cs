f5011bb315d8eb548e76e3f5c559893bb4287657
Dogora <Ddogora@gmail.com>
2022-02-11 20:09:43 +0300

    26 задача

--- Tasks/Block-3/taks26/Program.cs
+++ Tasks/Block-3/taks26/Program.cs
@@ -3,3 +3,14 @@
 
 using static System.Console;
 
+int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
+int temp = 1;
+WriteLine( $"Число A = {A}");
+WriteLine( $"Число B = {B}");
+for(int i = 1; i< B+1; i++)
+{
+    temp= temp *A;
+
+}
+WriteLine( $" Число А в натуральной степени В = {temp}");
