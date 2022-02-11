14240ce2ec450e649015ff73bb11af7aa3b31632
Dogora <Ddogora@gmail.com>
2022-02-11 23:43:49 +0300

    31 задача

---
+++ Tasks/Block-4/task31/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 31
+// Задать массив из 8 элементов и вывести их на экран
+
+using static System.Console;
+
+int[] numbers = new int[8];
+for (int d = 0; d < 8; d++)
+{
+    numbers[d] = new Random().Next(1, 99);
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
