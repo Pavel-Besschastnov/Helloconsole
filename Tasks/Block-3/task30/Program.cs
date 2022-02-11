97e7562693fc62e2ee1a1507f38063fd5880d1bb
Dogora <Ddogora@gmail.com>
2022-02-11 23:15:16 +0300

    30  задача

---
+++ Tasks/Block-3/task30/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 30
+// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+using static System.Console;
+
+int[] nums = new int[] { 213, 213, 4,32,17,70,2,34,41 };
+int a = 0;
+for(int i=0; i<nums.Length;i++)
+{
+    if ( nums[i]%2 ==0)
+    {
+        a = nums[i]*nums[i]*nums[i];
+    WriteLine($"Куб четнjuj числа {nums[i]} :{a}");
+    }
+
+
+}
