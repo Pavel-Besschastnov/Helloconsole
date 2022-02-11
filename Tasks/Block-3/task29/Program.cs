f72b5362b304c79d6739d6a80e7edaabc49557fe
Dogora <Ddogora@gmail.com>
2022-02-11 22:52:54 +0300

    29 ЗАДАЧА

---
+++ Tasks/Block-3/task29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29
+//Написать программу вычисления произведения чисел от 1 до N
+
+using static System.Console;
+
+int numberN = new Random().Next(1,20);
+int pro= 1;
+WriteLine(numberN );
+for ( int i =1 ; i<numberN;i++)
+{
+    pro = pro * i;
+    WriteLine($"{pro} * {i} = {pro}");
+}
