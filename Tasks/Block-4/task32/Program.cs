ff4dc2151afd03ac59f1509bc405e6a474a32e75
Dogora <Ddogora@gmail.com>
2022-02-11 23:51:50 +0300

    32  задача

---
+++ Tasks/Block-4/task32/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 32
+// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+using static System.Console;
+
+int[] numbers = new int[8];
+for (int d = 0; d < 8; d++)
+{
+    numbers[d] = new Random().Next(0,2);
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
