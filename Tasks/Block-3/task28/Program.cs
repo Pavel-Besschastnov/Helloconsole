82fbda5240baf2c1dc1071d39f07acf6ab2ca8e4
Dogora <Ddogora@gmail.com>
2022-02-13 06:03:59 +0300

    28 задача

---
+++ Tasks/Block-3/task28/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 28
+// Подсчитать сумму цифр в числе
+
+using static System.Console;
+
+String ss = ReadLine();
+int sum = 0;
+for (int i = 0; i < ss.Length; i++)
+{
+    sum = sum + int.Parse(ss[i].ToString());
+}
+WriteLine(sum);
