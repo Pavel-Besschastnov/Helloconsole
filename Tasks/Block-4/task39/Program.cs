0f73348c599e896a7489daa3332a2ec6e5af2ac4
Dogora <Ddogora@gmail.com>
2022-02-19 20:50:36 +0300

    39 задача

---
+++ Tasks/Block-4/task39/Program.cs
@@ -0,0 +1,21 @@
+﻿ // Задача 39
+ // Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+using static System.Console;
+int[] numb=new int[10];
+for(int i=0;i<10;i++)
+{
+numb[i]=new Random().Next(1,10);
+Console.Write($" {numb[i]}");
+}
+Console.WriteLine();
+Console.WriteLine();
+int summ=0;
+int a= 0;
+int b= numb.Length-1;
+while( a<b-1)
+{
+summ= numb[a]*numb[b];
+Console.Write($" {summ}");
+a++;
+}
