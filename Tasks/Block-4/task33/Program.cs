28e80cb8e742200b0f8c162dfb3e88d6b9d42f0b
Dogora <Ddogora@gmail.com>
2022-02-19 18:49:26 +0300

    34 задача

---
+++ Tasks/Block-4/task33/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 33
+// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+using static System.Console;
+
+int[] array = new int[12];
+for (int i = 0; i < 12; i++)
+{
+    array[i] = new Random().Next(0, 9);
+    Console.Write($" { array[i] }");
+}
+Console.WriteLine();
+int chet = 0;
+int nechet = 0;
+for (int j = 0; j < 12; j++)
+{
+    if (array[j] % 2 == 0) chet++;
+    else nechet++;
+}
+Console.WriteLine($" Количество четных символов в массиве ={chet}");
+Console.WriteLine($" Количество нечетных символов в массиве = {nechet}");
---
+++ Tasks/Block-4/task34/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34
+// Написать программу замену элементов массива на противоположные
+
+int[] massiv = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    massiv[i] = new Random().Next(-10, 10);
+    Console.Write($" {massiv[i]}");
+}
+Console.WriteLine();
+for (int j = 0; j < 10; j++)
+{
+
+    massiv[j] = massiv[j] * -1;
+}
+foreach (int d in massiv)
+{
+    Console.Write("{0} ", d);
+}
