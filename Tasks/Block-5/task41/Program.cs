97e0b9487fa32e12487bc97e0cbad60f040f78fd
Dogora <Ddogora@gmail.com>
2022-02-20 00:16:49 +0300

    41 задача

---
+++ Tasks/Block-5/task41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41
+//  Выяснить являются ли три числа сторонами треугольника
+using static System.Console;
+
+int a = int.Parse(ReadLine());
+int b = int.Parse(ReadLine());
+int c = int.Parse(ReadLine());
+
+if (a * a + b * b == c * c) WriteLine("Введенные числа являются треугольником");
+if (a * a + c * c == b * b) WriteLine("Введенные числа являются треугольником");
+if (b * b + c * c == a * a) WriteLine("Введенные числа являются треугольником");
+if((a * a + b * b != c * c) && (a * a + c * c != b * b) && (b * b + c * c != a * a))WriteLine(" Введенные числа не являются треугольником");
+ 
