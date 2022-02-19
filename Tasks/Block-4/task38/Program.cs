d79896acc3f316e720ebe5d821cd32e51500bea7
Dogora <Ddogora@gmail.com>
2022-02-19 20:46:23 +0300

    38 задача

---
+++ Tasks/Block-4/task38/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 38
+// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+using static System.Console;
+int[] massiv = new int[123];
+for (int i = 0; i < 123; i++)
+{
+    massiv[i] = new Random().Next(0, 300);
+    Console.Write($" {massiv[i]}");
+}
+Console.WriteLine();
+int summ = 0;
+for (int j = 0; j < massiv.Length; j++)
+{
+    if (j % 2 != 0) summ = summ + massiv[j];
+
+}
+Console.WriteLine($"Сумма элементов стоящих на нечетной позиции = {summ}");
