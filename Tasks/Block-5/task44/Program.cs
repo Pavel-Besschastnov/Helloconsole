6e64e5aed78556100a8203b964095afc140d2c8d
Dogora <Ddogora@gmail.com>
2022-02-21 22:15:14 +0300

    44 задача

---
+++ Tasks/Block-5/task44/Program.cs
@@ -0,0 +1,14 @@
+﻿// 44 задача
+// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+double k1= new Random().Next(1,20);
+double k2= new Random().Next(1,20);
+double b1= new Random().Next(1,20);
+double b2= new Random().Next(1,20);
+Console.WriteLine($"k1 = {k1} k2 = {k2} b1 = {b1} b2 = {b2}");
+double x;
+double y;
+x = (b1 - b2)/ (k2- k1);
+y = k2*x+ b2;
+Console.WriteLine( "При таких координатах точка пересечения будет с этими координатами");
+Console.WriteLine($"x = {x} y = {y}");
