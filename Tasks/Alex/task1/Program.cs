98b6a33ce74efd3f06d741483d9447529ddd38f4
Dogora <Ddogora@gmail.com>
2022-02-21 22:08:21 +0300

    42 задача

---
+++ Tasks/Alex/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿  // Задача 48
+  //  Показать двумерный массив размером m×n заполненный целыми числами
+using static System.Console;
+Console.Clear();
+int m = int.Parse(ReadLine());
+int n = int.Parse(ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int d = 0; d < n; d++)
+    {
+        array[i, d] = new Random().Next(10, 100);
+    }
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Write($" ( {array[i, j]} )");
+    }
+    WriteLine();
+}
--- Tasks/Block-5/task42/Program.cs
+++ Tasks/Block-5/task42/Program.cs
@@ -2,3 +2,16 @@
 // Определить сколько чисел больше 0 введено с клавиатуры
 using static System.Console;
 
+Console.WriteLine("Введите числа:");
+string stroka= Console.ReadLine();
+int count=1;
+byte backnum=0;
+foreach(char a in stroka)
+{
+int temp=Convert.ToByte(a);
+if((byte)a==32 && (byte)a!=backnum) count++;
+backnum=(byte)a;
+
+
+}
+Console.WriteLine($"количество введенных чисел = {count}");
