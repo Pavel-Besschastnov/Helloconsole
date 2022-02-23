2c3435dc1207ad523649ca8422eaf69f27a77bd2
Dogora <Ddogora@gmail.com>
2022-02-23 22:27:22 +0300

    51 задача

---
+++ Tasks/Block-6/task51/Program.cs
@@ -0,0 +1,24 @@
+﻿// 51 Задача
+// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+Console.WriteLine("Введите количество строк :");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк :");
+int n = int.Parse(Console.ReadLine());
+int[,] massiv = new int[m, n];
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        massiv[i, j] = i + j;
+    }
+}
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        Console.Write($" {massiv[i, j]}");
+    }
+}
+Console.WriteLine();
