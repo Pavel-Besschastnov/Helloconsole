92709b0dc7c8d77374c10b72809b0b7470fd63c9
Dogora <Ddogora@gmail.com>
2022-02-24 20:48:49 +0300

    58 задача

---
+++ Tasks/Block-6/task58/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 58
+// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).
+
+Console.WriteLine("Введите количество строк :");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов :");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write($" {array[i, j]}");
+        }
+    }
+}
+int temp = 0;
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    temp = array[0, j];
+    array[0, j] = array[n - 1, j];
+    array[n - 1, j] = temp;
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine();
+    
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           
+            Console.Write($" {array[i, j]}");
+        }
+    }
+}
+Console.WriteLine();
