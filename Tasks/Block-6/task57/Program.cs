6491c043bd58b0a94c28a25c75ff63b14977c7cf
Dogora <Ddogora@gmail.com>
2022-02-27 15:04:25 +0300

    57 задача

---
+++ Tasks/Block-6/task57/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 57
+// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+Console.WriteLine("Введите количество строк :");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов :");
+int n = int.Parse(Console.ReadLine());
+int[,] massiv = new int[m, n];
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        massiv[i, j] = new Random().Next(1, 10);
+        Console.Write($"{massiv[i, j]} ");
+
+    }
+
+}
+
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+
+
+
+    for (int d = 0; d < massiv.GetLength(1); d++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            if (j != massiv.GetLength(1) - 1)
+
+            {
+                if (massiv[i, j] > massiv[i, j + 1])
+                {
+                    int minstrok = massiv[i, j + 1];
+                    massiv[i, j + 1] = massiv[i, j];
+                    massiv[i, j] = minstrok;
+
+                }
+            }
+        }
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        Console.Write($"{massiv[i, j]} ");
+    }
+}
+
+
+
+
+
+
+
