c9ba35a32c865932e417f50ef65582bde8b246de
Dogora <Ddogora@gmail.com>
2022-03-01 21:31:46 +0300

    59 задача

---
+++ Tasks/Block-6/task59/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 59
+// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите количество строк :");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов :");
+int n = int.Parse(Console.ReadLine());
+if (m == n)
+{
+    Console.WriteLine("Выполнение невозможно");
+    return;
+}
+int[,] massiv = new int[m, n];
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        massiv[i, j] = new Random().Next(1, 10);
+        Console.Write($"{massiv[i, j]} ");
+
+    }
+
+}
+Console.WriteLine();
+int summ = 0;
+int minin = summ;
+int s = 0;
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+
+    if (summ != 0 && minin > summ)
+    {
+        minin = summ;
+        s = i;
+
+    }
+    summ = 0;
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        summ = summ + massiv[i, j];
+        if (i == 0) minin = summ;
+
+    }
+}
+Console.WriteLine($" Наименьшая сумма элементов в строке №{s} и равна : {minin}");
